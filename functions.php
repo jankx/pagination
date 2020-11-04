6aa6aaccddde46406d41732ec7f0f8e1ea588add
Puleeno Nguyen <puleeno@gmail.com>
2020-11-04 13:31:09 +0700

    create wrapper for paginate

--- functions.php
+++ functions.php
@@ -1,5 +1,9 @@
 <?php
 function jankx_paginate() {
     // Create alias of paginate_links
-    return paginate_links();
+    ob_start();
+    echo '<div class="jankx-pagination">';
+    echo paginate_links();
+    echo '</div>';
+    return ob_get_clean();
 }
