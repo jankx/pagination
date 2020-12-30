d9f051f8c53bfd149245ed13408d2ae0893599bb
Puleeno Nguyen <puleeno@gmail.com>
2020-12-30 08:53:12 +0700

    hide next and prev text

--- functions.php
+++ functions.php
@@ -3,7 +3,10 @@ function jankx_paginate() {
     // Create alias of paginate_links
     ob_start();
     echo '<div class="jankx-pagination">';
-    echo paginate_links();
+    echo paginate_links(array(
+        'prev_text' => '',
+        'next_text' => '',
+    ));
     echo '</div>';
     return ob_get_clean();
 }
