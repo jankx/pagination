217a8a644c2cb03f203a6fb42689e784df77ba54
Puleeno Nguyen <puleeno@gmail.com>
2021-01-04 12:47:30 +0700

    Only show pagination when pagination has pages

--- functions.php
+++ functions.php
@@ -1,12 +1,12 @@
 <?php
 function jankx_paginate() {
     // Create alias of paginate_links
-    ob_start();
-    echo '<div class="jankx-pagination">';
-    echo paginate_links(array(
+    $pagination = paginate_links(array(
         'prev_text' => jankx_template('common/pagination/prev', array(), null, false),
         'next_text' => jankx_template('common/pagination/next', array(), null, false),
     ));
-    echo '</div>';
-    return ob_get_clean();
+    if (!$pagination) {
+        return;
+    }
+    return sprintf('<%1$s class="jankx-pagination">%2$s</%1$s>', 'div', $pagination);
 }
