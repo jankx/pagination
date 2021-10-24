aaa248876c5366da9abf8af51b57182187d2be3b
Puleeno Nguyen <puleeno@gmail.com>
2021-10-24 17:47:48 +0700

    Support custom wp_query to paginate

--- functions.php
+++ functions.php
@@ -1,10 +1,21 @@
 <?php
-function jankx_paginate() {
+function jankx_paginate($need_paginate_query = null) {
     // Create alias of paginate_links
+    if (!is_null($need_paginate_query)) {
+        global $wp_query;
+        $backup_wp_query = $wp_query;
+        $wp_query = $need_paginate_query;
+    }
+
     $pagination = paginate_links(array(
         'prev_text' => jankx_template('common/pagination/prev', array(), null, false),
         'next_text' => jankx_template('common/pagination/next', array(), null, false),
     ));
+
+    if (!is_null($need_paginate_query)) {
+        $wp_query = $backup_wp_query;
+    }
+
     if (!$pagination) {
         return;
     }
